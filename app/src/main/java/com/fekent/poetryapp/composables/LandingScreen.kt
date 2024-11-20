2b49aa27b3981cfdb9b2687a4437fa09394b8281
FeKent <kentf022@gmail.com>
2024-11-20 01:00:27 +0000

    Add framework for Home/Landing Screen

---
+++ app/src/main/java/com/fekent/poetryapp/composables/LandingScreen.kt
@@ -0,0 +1,17 @@
+package com.fekent.poetryapp.composables
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LandingScreen() {
+    LandingScreenUI()
+}
+
+@Composable
+private fun LandingScreenUI() {
+    Column {
+        Text(text = "BOO")
+    }
+}
