1a15a3965407c4679b28c5645570c5e4b6f666ca
FeKent <kentf022@gmail.com>
2025-01-03 17:02:41 +0000

    Update indicator color for navigation bar items

--- app/src/main/java/com/fekent/poetryapp/MainActivity.kt
+++ app/src/main/java/com/fekent/poetryapp/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -156,7 +157,10 @@ fun NavigationBarView(currentRoute: String?, onClick: (String) -> Unit) {
                         fontWeight = FontWeight.Bold,
                         color = MaterialTheme.colorScheme.primary
                     )
-                }
+                },
+                colors = NavigationBarItemDefaults.colors(indicatorColor = Color.Transparent)
+                // indicator color used for accessibility - changed for me only
+
             )
         }
     }
