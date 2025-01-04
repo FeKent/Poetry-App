e17604100d428b43bf6d915e5ecdbda179445d56
FeKent <kentf022@gmail.com>
2025-01-04 21:55:55 +0000

    Add example lists for each data class

--- app/src/main/java/com/fekent/poetryapp/data/PoemDataClasses.kt
+++ app/src/main/java/com/fekent/poetryapp/data/PoemDataClasses.kt
@@ -10,10 +10,22 @@ data class Authored(
     val poem: String
 )
 
+val authoredExample = listOf(
+    Authored(0, "Disturbia", "Late September in the city"),
+    Authored(1, "Messy", "Cause I'm too messy, then I'm too fucking clean")
+)
+
+
+
 @Entity
 data class Saved(
     @PrimaryKey (autoGenerate = true) val id: Int = 0,
     val title: String?,
     val poem: String,
     val author: String
+)
+
+val savedExamples = listOf(
+    Saved(0, "Duckie", "Rollies? Dice Rollies?", "Ciprian"),
+    Saved(0, "Nobody", "Nobody, Nobody, No body", "Mitski")
 )
