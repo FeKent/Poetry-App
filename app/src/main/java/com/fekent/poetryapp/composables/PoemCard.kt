b21299728d17b2623a818eb41fd388395bab2bd5
FeKent <kentf022@gmail.com>
2025-01-14 17:00:36 +0000

    Add styling to PoemCard

--- app/src/main/java/com/fekent/poetryapp/composables/PoemCard.kt
+++ app/src/main/java/com/fekent/poetryapp/composables/PoemCard.kt
@@ -13,11 +13,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fekent.poetryapp.data.Authored
 import com.fekent.poetryapp.data.Saved
 import com.fekent.poetryapp.ui.theme.PoetryAppTheme
+import com.fekent.poetryapp.ui.theme.abeezeeFont
+import com.fekent.poetryapp.ui.theme.aboretoFont
 
 @Composable
 fun PoemCard(
@@ -31,14 +36,18 @@ fun PoemCard(
     ) {
         Column(
             modifier = Modifier
-                .padding(4.dp).fillMaxWidth()
+                .padding(4.dp)
+                .fillMaxWidth()
         ) {
             Spacer(Modifier.size(16.dp))
             if (authored != null || saved != null) {
                 (authored?.title ?: saved?.title)?.let {
                     Text(
                         text = it,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        fontFamily = aboretoFont,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 24.sp
                     )
                 }
             }
@@ -46,12 +55,13 @@ fun PoemCard(
             Box(
                 modifier = Modifier
                     .weight(1f, fill = false)
-                    .verticalScroll(rememberScrollState()).fillMaxWidth(),
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ) {
                 if (authored != null || saved != null) {
                     (authored?.poem ?: saved?.poem)?.let {
-                        Text(text = it )
+                        Text(text = it, fontFamily = abeezeeFont, fontWeight = FontWeight.Light)
                     }
                 }
             }
@@ -59,7 +69,10 @@ fun PoemCard(
             if (saved != null) {
                 Text(
                     text = saved.author,
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontFamily = aboretoFont,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 16.sp
                 )
                 Spacer(Modifier.size(16.dp))
             }
@@ -71,16 +84,18 @@ fun PoemCard(
 @Composable
 private fun PoemScreenPreview() {
     PoetryAppTheme {
-        PoemCard(authored = null, saved = Saved(
-            0, "Fire and Ice", "Some say the world will end in fire,\n" +
-                    "Some say in ice.\n" +
-                    "From what I’ve tasted of desire\n" +
-                    "I hold with those who favor fire.\n" +
-                    "But if it had to perish twice,\n" +
-                    "I think I know enough of hate\n" +
-                    "To say that for destruction ice\n" +
-                    "Is also great\n" +
-                    "And would suffice.", "Robert Frost"
-        ))
+        PoemCard(
+            authored = null, saved = Saved(
+                0, "Fire and Ice", "Some say the world will end in fire,\n" +
+                        "Some say in ice.\n" +
+                        "From what I’ve tasted of desire\n" +
+                        "I hold with those who favor fire.\n" +
+                        "But if it had to perish twice,\n" +
+                        "I think I know enough of hate\n" +
+                        "To say that for destruction ice\n" +
+                        "Is also great\n" +
+                        "And would suffice.", "Robert Frost"
+            )
+        )
     }
 }
