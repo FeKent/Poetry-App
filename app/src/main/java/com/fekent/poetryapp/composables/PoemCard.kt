d9281d1f494b03a2380541bdb974610a57d1391c
FeKent <kentf022@gmail.com>
2025-01-15 00:44:03 +0000

    Experiment with gradient theming in PoemCard

--- app/src/main/java/com/fekent/poetryapp/composables/PoemCard.kt
+++ app/src/main/java/com/fekent/poetryapp/composables/PoemCard.kt
@@ -1,6 +1,8 @@
 package com.fekent.poetryapp.composables
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,22 +11,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fekent.poetryapp.data.Authored
 import com.fekent.poetryapp.data.Saved
 import com.fekent.poetryapp.ui.theme.PoetryAppTheme
-import com.fekent.poetryapp.ui.theme.abeezeeFont
 import com.fekent.poetryapp.ui.theme.aboretoFont
 
 @Composable
@@ -32,16 +40,39 @@ fun PoemCard(
     authored: Authored?,
     saved: Saved?,
 ) {
+    val gradientBrush = Brush.linearGradient(
+        colors = listOf(
+            MaterialTheme.colorScheme.primary,
+            MaterialTheme.colorScheme.primaryContainer
+        )
+    )
+    val textBrush = Brush.verticalGradient(
+        colors = listOf(
+            MaterialTheme.colorScheme.tertiary,
+            MaterialTheme.colorScheme.tertiaryContainer
+        )
+    )
+
     Card(
-        border = BorderStroke(4.dp, color = MaterialTheme.colorScheme.primaryContainer),
+//        border = BorderStroke(4.dp, color = MaterialTheme.colorScheme.primaryContainer),
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
         modifier = Modifier
+
             .padding(16.dp)
             .fillMaxWidth()
+            .border(
+                BorderStroke(
+                    width = 4.dp,
+                    brush = gradientBrush
+                ),
+                shape = RoundedCornerShape(10.dp)
+            )
             .shadow(
                 elevation = 5.dp,
                 spotColor = MaterialTheme.colorScheme.primaryContainer,
                 shape = RoundedCornerShape(10.dp)
             )
+            .background(gradientBrush, RoundedCornerShape(10.dp))
     ) {
         Column(
             modifier = Modifier
@@ -71,12 +102,19 @@ fun PoemCard(
             ) {
                 if (authored != null || saved != null) {
                     (authored?.poem ?: saved?.poem)?.let {
-                        Text(
-                            text = it,
-                            fontFamily = abeezeeFont,
-                            fontWeight = FontWeight.Light,
-                            color = MaterialTheme.colorScheme.primary
-                        )
+                        BasicText(text = buildAnnotatedString {
+                            withStyle(style = SpanStyle(brush = textBrush)) {
+                                append(
+                                    it
+                                )
+                            }
+                        })
+//                        Text(
+//                            text = it,
+//                            fontFamily = abeezeeFont,
+//                            fontWeight = FontWeight.Light,
+////                            color = MaterialTheme.colorScheme.primary
+//                        )
                     }
                 }
             }
